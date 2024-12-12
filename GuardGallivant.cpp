d7b30ff9c58211f5a590ea7924f0cc5af59177af
Gnarlsley <acowiguess@gmail.com>
2024-12-11 22:38:13 -0600

    Solved

--- GuardGallivant.cpp
+++ GuardGallivant.cpp
@@ -1,245 +1,219 @@
-#include <fstream>
-#include <string>
-#include <iostream>
-#include <unordered_set>
-#include <unordered_map>
-#include <cmath>
-#include <cstring>
+#include <fstream> //file pointers to read and write
+#include <iostream> //output to console
+#include <unordered_map> //used to count number of times a position was visited
+#include <unordered_set> //used to store number of unique positions
 
-#define right 1
-#define left -1
+//global constants dependant on the file dimensions
+#define startingPosition 5508
 #define up -132
 #define down 132
-#define startingPos 5508
-#define lineLength 132
-#define newLine 130
-#define fileSize 17160
+#define right 1
+#define left -1
+#define lineLength 130 //number of readable bytes per line
+#define lineEnd 132 //number bytes per line (includes \r\n)
+#define fileSize 17160 //total number of bytes in file
 
-//Example Data
-// #define ending 117
-// #define lineLength 12
-// #define lineCount 10
-// #define right 1
-// #define left -1
-// #define up -12
-// #define down 12
-// #define startingPos 76
-// #define fileSize 118
-// #define newLine 10
+//struct definition to hold a position and a direction
+struct PositionDirectionPair {
+    std::streamoff position;
+    std::streamoff direction;
 
-std::unordered_map<std::streamoff, int> visitedPositions;
+    bool operator==(const PositionDirectionPair &other)const{
+        return position == other.position && direction == other.direction;
+    }
+};
+
+//hash is required to use unordered map datatype
+namespace std {
+    template <>
+    struct hash<PositionDirectionPair> {
+        size_t operator()(const PositionDirectionPair &pair)const {
+            return hash<std::streamoff>()(pair.position)^
+                hash<std::streamoff>()(pair.direction);
+        }
+    };
+}
+
+
+std::fstream file("puzzle.txt", std::ios::binary | std::ios::in | std::ios::out);
+bool escaped = false;
+char obstacle = '#';
+std::streamoff direction = up;
+std::unordered_set<std::streamoff> uniquePositions;
 std::unordered_set<std::streamoff> validObstaclePositions;
 
-bool inBounds(int direction, std::fstream &file){
 
-    int currPos = file.tellg();
-
-    //check if pointer currently on \r\n characters
-    if(direction == right || direction == left){
-        int horizontalBound = newLine;
-        while(horizontalBound <= currPos){
-            if(currPos == horizontalBound || currPos == horizontalBound + 1){
-                return false;
-            }
-            horizontalBound += lineLength;
-        }
-    }
-
-    //check if pointer past beginning/end
-    if(currPos < 0 || currPos >= fileSize){
-        file.clear();
-        return false;
-    }
-
-    
-    return true;
-}
-
-int rotate(int direction){
-        switch(direction){
-            case up:
-                return right;
-                break;
-            case right:
-                return down;
-                break;
-            case down:
-                return left;
-                break;
-            case left:
-                return up;
-                break;
-            default:
-                return direction;
-                break;
-        }
-}
-
-bool canMove(std::streamoff direction, std::fstream &file){
-    std::streampos currPos = file.tellg();
-    std::streampos newPos = currPos + direction;
-    file.seekg(newPos, std::ios::beg);
-    if(file.fail()){
-        file.clear();
-        return true;
-    }
-        char ch = file.peek();
-
-        if(ch == '#'){
-            //found obstacle, move back to original position
-            file.seekg(currPos, std::ios::beg);
-            return false;
-        }
-
-        //no obstacle, move character
-        return true;
-}
-
-int solvePartOne(){
-    std::fstream file("puzzle.txt", std::ios::in | std::ios::binary);
-    std::streamoff direction = up;
-    int count = 0;
-    if(!file.is_open()){
-        return 1;
-    }
-
-    //move pointer to character position
-    file.seekg(startingPos, std::ios::beg);
-    //add the start position to the list of vistied positions
-    visitedPositions[file.tellg()]++;
-
-    //commence movement
-    while(true){
-
-        if(file.fail()){
-            std::cout << "Error details: " << strerror(errno);
-        }
-        //first check if maze is complete
-        if(!inBounds(direction, file)){
-            break;
-        }
-
-        //then check if you've been on this square before
-        if (visitedPositions.find(file.tellg()) == visitedPositions.end()) {
-            visitedPositions[file.tellg()]++;
-        }
-
-        //then move
-        //the canMove function both determines if movement is allowed AND moves the pointer
-        if (!canMove(direction,file)){
-            //if unable to move, "rotate" the pointer
-            direction = rotate(direction);
-        }
-    }
-    std::cout<<"Number of distinct positions: ";
-    std::cout<<visitedPositions.size();
-
-    file.close();
-    return 0;
-}
-
-int getFileSize(){
+void getFileSize(){
     std::ifstream file("puzzle.txt", std::ios::binary | std::ios::ate);
-    std::streamoff size = file.tellg();
+     std::streamoff size = file.tellg();
     file.close();
 
     std::cout<<"File size: " << size << "bytes";
-    return 0;
 }
-int main() {
-    getFileSize();
-     std::fstream file("puzzle.txt", std::ios::in | std::ios::out | std::ios::binary);
 
-     std::streamoff direction = up;
-     int count = 0;
-     const char obstacle = '#';
-     if(!file.is_open()){
-         return 1;
-     }
+//guard rotates 90 degrees right if met with an obstacle
+std::streamoff rotate(std::streamoff direction){
+        switch(direction){
+            case up:
+                return right;
+            case right:
+                return down;
+            case down:
+                return left;
+            case left:
+                return up;
+            default:
+                return direction;
+        }
+}
 
-     //move pointer to character position
-     file.seekg(startingPos, std::ios::beg);
-     //add the start position to the list of vistied positions
-     visitedPositions[file.tellg()]++;
+bool canMove(std::streamoff direction){
+    std::streampos originalPosition = file.tellg();
+    std::streampos newPosition = originalPosition + direction;
 
-     while(true){
-  
-        //first check if maze is complete
-        if(!inBounds(direction, file)){
+    //if new position is out of bounds, seeking to it will throw an error
+    file.seekg(newPosition, std::ios::beg);
+    
+    //errors are handled here, if the pointer leaves the file, the guard left the maze
+    if(file.fail()){
+        file.clear();
+        escaped = true;
+        return true;
+    }
+
+    //another check to see if the new position is out of bounds of the file
+    if(newPosition < 0 || newPosition >= fileSize){
+        file.clear();
+        escaped = true;
+        return true;
+    }
+
+    //if not out of bounds, read the character
+    char c = file.peek();
+
+    //character escaped on the left or right side
+    if(c == '\001' || c == '\r'){
+        escaped = true;
+        return true;
+    }
+
+    //move pointer back
+    file.seekg(originalPosition);
+    
+    return (c != obstacle);
+}
+
+void move(std::streamoff direction){
+    file.seekg(direction, std::ios::cur);
+}
+
+void restartMaze(){
+    escaped = false;
+    direction = up;
+    file.seekg(startingPosition);
+}
+
+void solvePartOne(){
+     if(file.fail()){
+        file.close();
+        std::cout<<"Failure to open file.\n";
+    }
+    
+    //move pointer to guard's position
+    file.seekg(startingPosition);
+    //add the starting position to the unordered map
+    uniquePositions.insert(file.tellg());
+
+    while(!escaped){
+        if(file.fail()){
+            std::cout<<"Failure during maze run. Exiting...\n";
             break;
         }
 
-        if(file.fail()){
-            std::cout << "Error details: " << strerror(errno);
-        }
-
-        //check if you've been on this square before
-        visitedPositions[file.tellg()]++;
-        //move
-        //the canMove function both determines if movement is allowed AND moves the pointer
-        if (!canMove(direction,file)){
-            //if unable to move, "rotate" the pointer
+        if(canMove(direction)){
+            move(direction);
+        } else {
             direction = rotate(direction);
         }
+        if(!escaped){
+            //if still in the maze, add the new position direction pair to the list
+            //if escaped squares were added, errors would arise
+            uniquePositions.insert(file.tellg());
+        }
     }
-    std::cout<<visitedPositions.size();
-    std::cout<<"\n";
-   for (int pos = 0; pos < fileSize; pos++){
-         direction = up;
-         //std::streampos pos = obsPos.first;
-        if(pos == startingPos){
-             continue;
-         }
-            file.seekp(pos,std::ios::beg);
-            file.seekg(pos,std::ios::beg);
+
+    std::cout<<"Answer for part one: " << uniquePositions.size() << "\n\n";
+}
+
+void solvePartTwo(){
+    
+    std::cout<<"Running the maze "<< uniquePositions.size() - 1 << " times. Please wait...\n\n";
+
+    int numOfObstacles = 0;
+
+    //remove the starting position from the list, no need to add an obstacle on top of the guard
+    uniquePositions.erase(5508);
+    
+    //for loop to go through every position in the previous list
+    for(const auto &entry : uniquePositions){
+
+        //list to keep track of visited positions, the direction, and how many times visited
+        std::unordered_map<PositionDirectionPair, int> loopTestPositions;
+
+        //move write pointer to the new position
+        file.seekp(entry);
+        //replace the open space with an obstacle
+        file.put(obstacle);
+        file.flush();
+
+        //reset escaped,direction, and read pointer
+        restartMaze();
+        //add position to list
+        loopTestPositions[{file.tellg(), direction}]++;
+
+        while(!escaped){
             if(file.fail()){
-                std::cerr << "Error moving write pointer to position: " << pos << "\n";
-                file.clear();
+            std::cout<<"Failure during maze run. Exiting...\n";
+            break;
             }
-            char original = file.peek();
-            file.put(obstacle);
-            file.seekg(startingPos, std::ios::beg);
-            std::unordered_map<std::streamoff, int> positionVisitCount;
 
-            //do the movement test, check if looping
-            while(true){
-                std::streampos currPos = file.tellg();
-                //add position to list
-                positionVisitCount[currPos]++;
-
-                //check if location has been visited more than 5 times
-                if(positionVisitCount[currPos] > 500){
-                    std::cout << "Found a position visited more than 5 times, loop found: ";
-                    std::cout << pos;
-                    std::cout << "\n";
-                    validObstaclePositions.insert(pos);
+            if(canMove(direction)){
+                //after moving, break if escaped. Do not want to add the position to the list
+                if(escaped){
                     break;
                 }
-
-                //move
-                //the canMove function both determines if movement is allowed AND moves the pointer
-                if (!canMove(direction,file)){
-                    //if unable to move, "rotate" the pointer
-                    direction = rotate(direction);
-                }
-
-                //if maze is escapable
-                if(!inBounds(direction, file)){
-                    if(file.fail()){
-                        file.clear();
-                    }   
-                    std::cout<<"No loop: "<<pos<<"\n";
-                    break;
-                }
+                move(direction);
+            } else {
+                direction = rotate(direction);
             }
-            file.seekp(pos, std::ios::beg);
-            file.put(original);
-        // std::cout<<"Replaced character at: ";
-        // std::cout<<file.tellp();
-        // std::cout<<" with a .";
-        // std::cout<<"\n";
-   }
-    std::cout<<validObstaclePositions.size();
+
+            loopTestPositions[{file.tellg(), direction}]++;
+
+            //if guard visits a position facing the same direction twice, he is in a loop
+            if(loopTestPositions[{file.tellg(), direction}] > 1){
+                //add position to the list
+                validObstaclePositions.insert(entry);
+                break;
+            }
+
+            if(file.tellg() >= fileSize){
+                std::cout<<"Exceeding filesize. Exiting...\n";
+                break;
+            }
+        }
+
+        //replace the object back to it's original state
+        file.seekp(entry);
+        file.put('.');
+        file.flush();
+    }
+
+    std::cout<<"Answer for part two: "<<validObstaclePositions.size()<<"\n";
     file.close();
+}
+
+int main(){
+    solvePartOne();
+    solvePartTwo();
     return 0;
 }
